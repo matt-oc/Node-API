bf72819fca7a9f828929a7633023e49c64c99c01
Matthew O'Connor <Matthew@mbp>
2021-05-18 21:40:44 +0100

    'fix bug causing score to not be corretly added in /addscore function'

--- server.js
+++ server.js
@@ -4,7 +4,16 @@ const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const storage = require('node-persist');
-let leaderboard = [];
+let leaderboard = [{"name": "Jess", "score":100},
+{"name": "John", "score": 92},
+{"name": "Peter", "score": 90},
+{"name": "Sarah", "score": 74},
+{"name": "Pa", "score": 70},
+{"name": "Jamie", "score": 59},
+{"name": "Helen", "score": 55},
+{"name": "Goodie", "score": 43},
+{"name": "Josh", "score": 42},
+{"name": "Alex", "score": 40}];
 app.use(express.static(__dirname + '/storage'));
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -18,9 +27,11 @@ app.get('/leaderboard', function(req, res) {
 // Add Score to leaderboard if top 10
 app.post('/addscore', function(req, res) {
   const score = req.body;
+  console.log(leaderboard[1].score)
   for (var i = 0; i < leaderboard.length; i++){
-    if (score.score > leaderboard[i] && i <= 10) {
+    if (score.score > leaderboard[i].score && i <= 10) {
       leaderboard.splice(i, 0, score);
+      leaderboard.splice(10,1)
       res.send('Congrats you got a top 10 score');
       store(leaderboard)
     }
@@ -28,8 +39,8 @@ app.post('/addscore', function(req, res) {
   res.send('Sorry your score didnt make it to the leaderboard');
 });
 
-app.listen(8000);
-console.log('Storage Server running on port 8000');
+app.listen(3002);
+console.log('Storage Server running on port 3002');
 
 async function store(item)  {
 
